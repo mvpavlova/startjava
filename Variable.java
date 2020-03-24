26904838fa5e34d8c44a56e669befc93ec1f0399
mvpavlova <54674959+mvpavlova@users.noreply.github.com>
2020-03-25 01:46:23 +0300

    'firsthw'

---
+++ Variable.java
@@ -0,0 +1,21 @@
+public class Variable{
+  public static void main(String[] args) {
+    byte a = 2;
+    short b = 4;
+    int c = 943;
+    long d = 8328;
+    float e = 23.40f;
+    double f =10.017763d;
+    char g = 'c';
+    boolean h = true;
+    System.out.println(a);
+    System.out.println(b);
+    System.out.println(c);
+    System.out.println(d);
+    System.out.println(e);
+    System.out.println(f);
+    System.out.println(g);
+    System.out.println(h);
+
+  }
+}

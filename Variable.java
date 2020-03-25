4bb83dbb7fc57af4df42f402c7c30262cf9cf2c8
mvpavlova <54674959+mvpavlova@users.noreply.github.com>
2020-03-25 17:12:05 +0300

    error fixed

---
+++ Variable.java
@@ -0,0 +1,21 @@
+public class Variable {
+    public static void main(String[] args) {
+        byte one = 2;
+        short two = 4;
+        int integer = 943;
+        long longer = 8328;
+        float real = 23.40f;
+        double doublereal = 10.017763d;
+        char letter = 'c';
+        boolean isbool = true;
+
+        System.out.println(one);
+        System.out.println(two);
+        System.out.println(integer);
+        System.out.println(longer);
+        System.out.println(real);
+        System.out.println(doublereal);
+        System.out.println(letter);
+        System.out.println(isbool);
+    }
+}

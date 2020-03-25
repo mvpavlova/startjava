93599d3576eb3f702ccc8ae8ee8913284c493e6e
mvpavlova <54674959+mvpavlova@users.noreply.github.com>
2020-03-25 16:56:55 +0300

    bugs fixed

--- Lesson1/Variable.java
+++ Lesson1/Variable.java
@@ -1,21 +1,21 @@
-public class Variable{
-  public static void main(String[] args) {
-    byte a = 2;
-    short b = 4;
-    int c = 943;
-    long d = 8328;
-    float e = 23.40f;
-    double f =10.017763d;
-    char g = 'c';
-    boolean h = true;
-    System.out.println(a);
-    System.out.println(b);
-    System.out.println(c);
-    System.out.println(d);
-    System.out.println(e);
-    System.out.println(f);
-    System.out.println(g);
-    System.out.println(h);
+public class Variable {
+    public static void main(String[] args) {
+        byte one = 2;
+        short two = 4;
+        int integer = 943;
+        long longer = 8328;
+        float real = 23.40f;
+        double doublereal = 10.017763d;
+        char letter = 'c';
+        boolean isbool = true;
 
-  }
+        System.out.println(one);
+        System.out.println(two);
+        System.out.println(integer);
+        System.out.println(longer);
+        System.out.println(real);
+        System.out.println(doublereal);
+        System.out.println(letter);
+        System.out.println(isbool);
+    }
 }

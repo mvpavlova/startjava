8242fee618a4f23c96a9e5f4d9eea1c748212b39
mvpavlova <54674959+mvpavlova@users.noreply.github.com>
2020-03-27 20:36:16 +0300

    Fixed

--- Lesson1/Calculator.java
+++ Lesson1/Calculator.java
@@ -2,29 +2,24 @@ public class Calculator {
     public static void main(String[] args) {
         int num1 = 10;
         int num2 = 2;
-        int result;
-        char op = '%';
-        if (op == '+') {
-            result = num1 + num2;
-            System.out.println(result);
-        } else if (op == '-') {
-            result = num1 - num2;
-            System.out.println(result);
-        } else if (op == '*') {
-            result = num1 * num2;
-            System.out.println(result);
-        } else if (op == '/') {
-            result = num1 / num2;
-            System.out.println(result);
-        } else if (op == '^') {
-            result = num1;
+        char operator = '^';
+
+        if (operator == '+') {
+            System.out.println(num1 + num2);
+        } else if (operator == '-') {
+            System.out.println(num1 - num2);
+        } else if (operator == '*') {
+            System.out.println(num1 * num2);
+        } else if (operator == '/') {
+            System.out.println(num1 / num2);
+        } else if (operator == '^') {
+             int result = num1;
             for (int i = 1; i < num2; i++) {
                 result = result * num1;
             }
             System.out.println(result);
-        } else if (op == '%') {
-            result = num1 % num2;
-            System.out.println(result);
+        } else if (operator == '%') {
+            System.out.println(num1 % num2);
         }
     }
 }

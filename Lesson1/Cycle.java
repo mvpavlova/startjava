111d85160039f236f64a1a31e2bd002a28ee1107
mvpavlova <54674959+mvpavlova@users.noreply.github.com>
2020-03-27 19:55:04 +0300

    change

--- Lesson1/Cycle.java
+++ Lesson1/Cycle.java
@@ -1,11 +1,11 @@
 public class Cycle {
     public static void main(String[] args) {
-        for(int i = 0; i <= 20; i++){
+        for(int i = 0; i <= 20; i++) {
             System.out.println(i);
         }
 
         int counter = -6;
-        while(counter <= 6){
+        while(counter <= 6) {
             System.out.println(counter);
             counter += 2;
         }

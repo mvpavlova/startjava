368ce6f033a006f22cbd5168b87d9133ae99f5e5
mvpavlova <54674959+mvpavlova@users.noreply.github.com>
2020-03-27 20:55:40 +0300

    Game

---
+++ Lesson1/MyFirstGame.java
@@ -0,0 +1,13 @@
+public class MyFirstGame {
+    public static void main(String[] args) {
+        int numberComputer = 20;
+        int numberUser = 20;
+        if (numberUser > numberComputer){
+            System.out.println("Введенное вами число больше того, что загадал компьютер");
+        } else if (numberUser < numberComputer) {
+              System.out.println("Введенное вами число меньше того, что загадал компьютер");
+        } else if (numberUser == numberComputer) {
+              System.out.println("Вы Угадали!");
+        } 
+    }
+}

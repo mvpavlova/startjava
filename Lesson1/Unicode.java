3c90fc95af5cafcb0322fc44ca32825b3110b849
mvpavlova <54674959+mvpavlova@users.noreply.github.com>
2020-03-28 03:17:36 +0300

    Unicode

---
+++ Lesson1/Unicode.java
@@ -0,0 +1,11 @@
+public class Unicode {
+    public static void main(String[] args) {
+        char unicode = '\u0033';
+        do{
+            System.out.println(unicode);
+            if(unicode <= 0126){
+                unicode++;
+            }
+        }while (unicode <= 0126);
+    }
+}

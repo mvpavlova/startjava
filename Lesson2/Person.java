1943753210acadf39e1afc56c73a31a85c970c2d
mvpavlova <54674959+mvpavlova@users.noreply.github.com>
2020-03-29 19:20:12 +0300

    lesson2.hw1

---
+++ Lesson2/Person.java
@@ -0,0 +1,30 @@
+public class Person {
+
+    public static void main(String[] args) {
+        String sex = "female";
+        String name = "Maria";
+        double height = 1.72;
+        double weight = 62.5;
+        int age = 24;
+    }
+
+    void walking() {
+
+    }
+
+    void sitting() {
+
+    }
+
+    void running() {
+
+    }
+
+    void speaking() {
+
+    }
+
+    void learningJava() {
+
+    }
+}

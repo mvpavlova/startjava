609791b38087412b899f8b9c5767950f15ca652e
mvpavlova <54674959+mvpavlova@users.noreply.github.com>
2020-03-29 19:25:21 +0300

    lesson2.hw1

--- Lesson2/Person.java
+++ Lesson2/Person.java
@@ -1,12 +1,10 @@
 public class Person {
 
-    public static void main(String[] args) {
-        String sex = "female";
-        String name = "Maria";
-        double height = 1.72;
-        double weight = 62.5;
-        int age = 24;
-    }
+    String sex = "female";
+    String name = "Maria";
+    double height = 1.72;
+    double weight = 62.5;
+    int age = 24;
 
     void walking() {
 

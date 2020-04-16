350e984c72a5b121a86e999dc65245d19e733feb
mvpavlova <54674959+mvpavlova@users.noreply.github.com>
2020-04-16 21:20:00 +0300

    Calculator hw4 correctv2

--- src/com/startjava/lesson_4/calculator/Calculator.java
+++ src/com/startjava/lesson_4/calculator/Calculator.java
@@ -1,24 +1,31 @@
 package com.startjava.lesson_4.calculator;
+
 import java.util.Scanner;
 import static java.lang.Integer.*;
 
 public class Calculator {
+
     private String[] calc;
-    public Calculator(String[] calc) {
-        this.calc = calc;
+    public Calculator(String[] splitMathExpression) {
+        this.calc = splitMathExpression;
     }
 
     public void calculate() {
+
         int num1 = parseInt(calc[0]);
         int num2 = parseInt(calc[2]);
         switch (calc[1]) {
-            case "+": System.out.println(num1 + num2);
+            case "+":
+                System.out.println(num1 + num2);
                 break;
-            case "-": System.out.println(num1 - num2);
+            case "-":
+                System.out.println(num1 - num2);
                 break;
-            case "*": System.out.println(num1 * num2);
+            case "*":
+                System.out.println(num1 * num2);
                 break;
-            case "/": System.out.println(num1 / num2);
+            case "/":
+                System.out.println(num1 / num2);
                 break;
             case "^":
                 int result = (int) Math.pow(num1, num2);
@@ -27,6 +34,19 @@ public class Calculator {
             case "%":
                 System.out.println(num1 % num2);
                 break;
+            case "max":
+                result = Math.max(num1, num2);
+                System.out.println(result);
+                break;
+            case "min":
+                result = Math.min(num1, num2);
+                System.out.println(result);
+                break;
+            case "hypot":
+                result = (int) Math.hypot(num1, num2);
+                System.out.println(result);
+                break;
+
         }
     }
 }
--- src/com/startjava/lesson_4/calculator/CalculatorTest.java
+++ src/com/startjava/lesson_4/calculator/CalculatorTest.java
@@ -4,19 +4,19 @@ import java.util.Scanner;
 
 public class CalculatorTest {
     public static void main(String[] args) {
-        Scanner choice = new Scanner(System.in);
+
+        Scanner scanner = new Scanner(System.in);
         String answer = "да";
         while(answer.equals("да")) {
             System.out.print("Введите математическое выражение: ");
-            Scanner scanner = new Scanner(System.in);
             String srcMathExpression = scanner.nextLine();
             String[] splitMathExpression = srcMathExpression.split(" ");
             Calculator calc = new Calculator(splitMathExpression);
             calc.calculate();
             do {
                 System.out.print("Хотите продолжить? да/нет ");
-                answer = choice.nextLine();
-            }while (!answer.equals("да") && !answer.equals("нет"));
+                answer = scanner.nextLine();
+            } while (!answer.equals("да") && !answer.equals("нет"));
         }
     }
 }
